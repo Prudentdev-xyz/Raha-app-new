76fc15861ab7f3d7ef04e3d3e1d8f3eb86fda3e9
AbdQoharr Olayiwola <olayiwolaabdqoharr226!gmail.com>
2025-11-19 15:44:19 -0800

    fix-Fundraise1

--- src/pages/FundraisePage.jsx
+++ src/pages/FundraisePage.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import React from "react";
 
 export const FundraisePage = () => {
   return (
-    <div>FundraisePage</div>
-  )
-}
+    <div className="flex">
+      <div>
+        <h1>Fundraise</h1>
+      </div>
+      <div className="bg-primary flex items-center justify-center">
+        <h1 className="text-black">Make a difference today</h1>
+        <p>
+          Join thousands of donors and organizations making an impact through
+          Raha.
+        </p>
+      </div>
+    </div>
+  );
+};
