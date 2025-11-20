40376980dee3bc79e9f10e06cbba82e18b0c0657
AbdQoharr Olayiwola <olayiwolaabdqoharr226!gmail.com>
2025-11-20 00:56:04 -0800

    fix-FooterFundraise

--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -14,7 +14,7 @@ export const FooterLinks = [
         text: "Our Story",
     },
     {
-      link: "/",
+      link: "/Fundraise",
       text: "Fundraise",
     },
     {
@@ -22,7 +22,7 @@ export const FooterLinks = [
       text: "Donate",
     },
     {
-      link: "/",
+      link: "/Fundraise",
       text: "Fundraise",
     },
     {
