98187f1fe160afc40bd435549f2705ba2478850a
AbdQoharr Olayiwola <olayiwolaabdqoharr226!gmail.com>
2025-11-19 17:22:30 -0800

    fix-fundraisePage-padding

--- src/pages/FundraisePage.jsx
+++ src/pages/FundraisePage.jsx
@@ -3,10 +3,10 @@ import React from "react";
 export const FundraisePage = () => {
   return (
     <div className="flex">
-      <div>
+      <div className="p-10">
         <h1>Fundraise</h1>
       </div>
-      <div className="bg-primary flex flex-col items-center justify-center">
+      <div className="bg-primary p-10 md:flex hidden flex-col items-center justify-center">
         <h1 className="text-secondary">Make a difference today</h1>
         <p className="text-white">
           Join thousands of donors and organizations making an impact through
