36d9a553f87be360e9df52c0130535ebc09effa5
AbdQoharr Olayiwola <olayiwolaabdqoharr226!gmail.com>
2025-11-21 18:34:13 -0800

    fix-About-pt

--- src/components/layouts/About.jsx
+++ src/components/layouts/About.jsx
@@ -1,7 +1,7 @@
 import React, { Children } from "react";
 
 export const About = () => {
-  return <div className="p-20 md:p-40 pt-20 md:pt-2 bg-primary relative">
+  return <div className="p-20 md:p-40 pt-100 md:pt-2 bg-primary relative">
     <h1 className="text-tetiary font-bold text-4xl md:text-8xl flex items-center justify-center text-center">About Us</h1>
     <p className="text-tetiary flex items-center justify-center text-center pt-1 text-sm md:text-xl">Home / About</p>
     <div>
