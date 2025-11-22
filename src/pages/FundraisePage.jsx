67f659be0feae91eadb143bdf13c72e7a63fe516
AbdQoharr Olayiwola <olayiwolaabdqoharr226!gmail.com>
2025-11-21 18:40:27 -0800

    fix-FundraisePage-pt

--- src/pages/FundraisePage.jsx
+++ src/pages/FundraisePage.jsx
@@ -12,7 +12,7 @@ export const FundraisePage = () => {
 
   return (
     <div>
-      <div className="p-10 flex flex-col items-center justify-center bg-gray-50">
+      <div className="p-10 flex flex-col items-center justify-center bg-gray-50 pt-30 md:pt-2">
         <img
           src={logo}
           alt="logo"
